2020cd123be91fee73d06daf15c82140b4c243a1
Kim Cable <findingkimcable@outlook.com>
2023-02-27 20:05:32 -0800

    Added Lists for Destinations, Restaurants, Transportation and Entertainment.

---
+++ main.py
@@ -0,0 +1,24 @@
+
+def run_day_trip_generator():
+
+
+def print_full_trip(list_of_options):
+
+
+def generate_random_item(list_of_items):
+    destinations = ["Los Angeles", "San Diego", "San Francisco", "Las Vegas", "Seattle"]
+    restaurants = ["Italian", "Mexican", "Sushi", "Steakhouse", "Buffet"]
+    modes_of_transportation = ["Car", "Bicycle", "Driver", "Walking", "Tour Bus"]
+    entertainment = ["Spa Trip", "City Tour", "Local Attraction-Zoo/Amusement Park/etc", "Show-Theartre/Comedy/etc", "Beach/Pool Day"]
+    
+
+
+def determine_satisfaction(current_trip, trip_options):
+
+
+def re_select_option(current_trip, options):
+
+
+
+run_day_trip_generator()
+
