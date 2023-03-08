960868d3e01d3b1a79fdd514dd84254e65ab9b35
Kimberly Cable <fingingkimcable@outlook.com>
2023-03-07 17:10:30 -0800

    I defined a function for randomly selecting a day trip, checking for satisfaction and reselecting if unsatisfied.

--- main.py
+++ main.py
@@ -1,43 +1,49 @@
 
-# def run_day_trip_generator:
+# Day Trip Project
 
 import random
 
 destination = ["Los Angeles", "San Diego", "San Francisco", "Las Vegas", "Seattle"]
 random_destination = random.choice(destination)
-print(random_destination)
+
 
 restaurant = ["Italian", "Mexican", "Sushi", "Steakhouse", "Buffet"]
 random_restaurant = random.choice(restaurant)
-print(random_restaurant)
+
 
 mode_of_transport = ["Car", "Bicycle", "Driver", "Walking", "Tour Bus"]
 random_mode_of_transport = random.choice(mode_of_transport)
-print(random_mode_of_transport)
+
 
 entertainment = ["Spa Trip", "City Tour", "Local Attraction-Zoo/Amusement Park/etc", "Show-Theartre/Comedy/etc", "Beach/Pool Day"]
 random_entertainment = random.choice(entertainment)
-print(random_entertainment)
-
-# def determine_satisfaction(current_trip, trip_options):
-
-
-current_trip = input("Are you satisfied with the current selections? Yes/No? ")
-if current_trip != "Yes":
-   print("Which item would you like to change? destination, restaurant, mode of transport or entertainment?")
-else:
-   print("Enjoy your trip!")
-
-# trip_options = re_select
 
 
 
-# def re_select_option(current_trip, options):
+# def determine_satisfaction(current_trip, )
+def trip_options():
+   while True:
+      
+      random_destination = random.choice(destination)
+      print(random_destination)
+
+      random_restaurant = random.choice(restaurant)
+      print(random_restaurant)
+
+      random_mode_of_transport = random.choice(mode_of_transport)
+      print(random_mode_of_transport)
+
+      random_entertainment = random.choice(entertainment)
+      print(random_entertainment)
+
+      user_choice = input("Are you satisfied with the current selections? Y/N? ")
+      if user_choice != "Y":
+         print("Let's try again!")
+      else:
+         print("Enjoy your trip!")
+         exit
+       
+      
+trip_options()
 
 
-
-# def print_full_trip(list_of_options):
-    
-
-
-# run_day_trip_generator()
