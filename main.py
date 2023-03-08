b29a3db8a8078dd0c59e4eacee69f2d97cbae389
Kimberly Cable <fingingkimcable@outlook.com>
2023-03-07 17:53:38 -0800

    Finished Day Trip Project

--- main.py
+++ main.py
@@ -1,26 +1,28 @@
 
 # Day Trip Project
 
+
 import random
 
+
 destination = ["Los Angeles", "San Diego", "San Francisco", "Las Vegas", "Seattle"]
-random_destination = random.choice(destination)
+# random_destination = random.choice(destination)
 
 
 restaurant = ["Italian", "Mexican", "Sushi", "Steakhouse", "Buffet"]
-random_restaurant = random.choice(restaurant)
+# random_restaurant = random.choice(restaurant)
 
 
 mode_of_transport = ["Car", "Bicycle", "Driver", "Walking", "Tour Bus"]
-random_mode_of_transport = random.choice(mode_of_transport)
+# random_mode_of_transport = random.choice(mode_of_transport)
 
 
 entertainment = ["Spa Trip", "City Tour", "Local Attraction-Zoo/Amusement Park/etc", "Show-Theartre/Comedy/etc", "Beach/Pool Day"]
-random_entertainment = random.choice(entertainment)
+# random_entertainment = random.choice(entertainment)
 
 
+# def determine_satisfaction(current_trip, trip options)
 
-# def determine_satisfaction(current_trip, )
 def trip_options():
    while True:
       
@@ -36,12 +38,15 @@ def trip_options():
       random_entertainment = random.choice(entertainment)
       print(random_entertainment)
 
+      current_trip = trip_options 
+
       user_choice = input("Are you satisfied with the current selections? Y/N? ")
       if user_choice != "Y":
          print("Let's try again!")
       else:
          print("Enjoy your trip!")
-         exit
+         return current_trip
+         
        
       
 trip_options()
